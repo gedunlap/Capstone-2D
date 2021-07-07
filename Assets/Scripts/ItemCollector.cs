a4eb8a18b045390e7ae662ecb1450f0338924b59
gedunlap <g.e.dunlap90@gmail.com>
2021-07-07 17:50:38 -0400

    adds apple counter

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    // var outside ontrigger to not reset count
+    private int apples = 0;
+
+    // var for updating apple count
+    [SerializeField] private Text applesText;
+
     // ontrigger referes to calling apple "is trigger"
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Apple"))
         {
             Destroy(collision.gameObject);
+            apples++;
+            applesText.text = "Apples: " + apples;
         }
     }
 }
